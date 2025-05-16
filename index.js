e4b4c7829aef7b153c075bf0b95d222928bab3ed
jero142 <180901023+jero142@users.noreply.github.com>
2025-05-15 22:30:11 -0400

    bubbles animations more on left

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const bubbleMaker = () => {
+
 // create bubble child of the body
 
 const bubble = document.createElement("span");
@@ -16,4 +18,12 @@ bubble.style.width = size;
 bubble.style.top = Math.random() * 100 + 50 + "%";
 bubble.style.left = Math.random() * 100 + "%"
 
+// bubbles animations more on the left
+const plusMinus = Math.random() > 0.5 ? 1 : -1;
 
+
+bubble.style.setProperty('--left', Math.random() * 100 * plusMinus + "%");
+
+};
+
+setInterval(bubbleMaker, 1000);
