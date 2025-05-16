56b36d4fa8b6d1810dee6c03b0801af467f402f9
jero142 <180901023+jero142@users.noreply.github.com>
2025-05-15 21:53:18 -0400

    randomized bubble position

--- index.js
+++ index.js
@@ -11,4 +11,9 @@ const size = Math.random() * 200 + 100 + "px";
 bubble.style.height = size;
 bubble.style.width = size;
 
+//  randomized bubble position
+
+bubble.style.top = Math.random() * 100 + 50 + "%";
+bubble.style.left = Math.random() * 100 + "%"
+
 
